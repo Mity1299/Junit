50b93a10fa4a91731bc4571befca9e60bc78801e
Mickey1299 <807798223@qq.com>
2019-05-21 21:34:09 +0800

    套件测试

---
+++ src/test/java/com/didispace/controller/AllTests.java
@@ -0,0 +1,19 @@
+/**
+* 
+* @author Mity1299
+*/
+package com.didispace.controller;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Mity1299
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ HouseControllerTest.class, UserControllerTest.class })
+public class AllTests {
+
+}
